a03b337fcf82f926e5223eb45ff00c56cb44e8f4
msochelpv <msoche@playvox.com>
2018-12-01 19:36:33 -0500

    create view for comparation audio

---
+++ build/precache-manifest.ac904cbe32bc85f43fb99ca773c9d9ee.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "36606aacea4ba71cf116",
+    "url": "/static/js/main.36606aac.chunk.js"
+  },
+  {
+    "revision": "dc687f26490bccf63d26",
+    "url": "/static/js/1.dc687f26.chunk.js"
+  },
+  {
+    "revision": "220ce002ab8319e4f60be76e18f32a38",
+    "url": "/index.html"
+  }
+];
---
+++ src/App.js
@@ -0,0 +1,171 @@
+import React, { Component } from 'react';
+import request from 'superagent';
+import LinearProgress from '@material-ui/core/LinearProgress';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import Send from '@material-ui/icons/Send';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+import TextField from '@material-ui/core/TextField';
+
+
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			resultsApi: null,
+			bigFile: null,
+			littleFile: null,
+			loading: false,
+			completed: 0
+		};
+	}
+
+	showResults = (base_url) => {
+		request
+		.get(base_url)
+		.set('Accept', 'application/json')
+		.then(res => {
+		this.setState({
+			resultsApi: res.body.result
+		});
+		})
+		.catch(err => {
+			console.log(err);
+		});
+	}
+
+	verifyProgress = (base_url) => {
+		request
+		.get(base_url)
+		.set('Accept', 'application/json')
+		.then(res => {
+			var percentage = parseInt(res.body.result.advanced[0]);
+			if (res.body.result.finished_at) {
+				console.log(this.timer, "DSADSADADAD")
+				this.setState({loading: false});
+				base_url = base_url.slice(0,-1) + "1";
+				this.showResults(base_url);
+				clearInterval(this.timer);
+			}
+		this.setState({
+			completed: percentage
+		});
+		})
+		.catch(err => {
+			console.log(err);
+		});
+	}
+
+	handleselectedBigFile = event => {
+		this.setState({
+			bigFile: event.target.files[0],
+		  loaded: 0,
+		})
+	}
+
+	handleselectedLittleFile = event => {
+		this.setState({
+			littleFile: event.target.files[0],
+		  loaded: 0,
+		})
+	}
+
+	handleUpload = () => {
+		var base_url = "https://audio-records-app.herokuapp.com/"
+		request
+		.post('https://audio-records-app.herokuapp.com/collection')
+		.attach('big_file', this.state.bigFile)
+		.attach('little_file', this.state.littleFile)
+		.then(res => {
+			base_url = base_url.concat(res.body.id, "?with_files=0");
+			this.timer = setInterval(this.verifyProgress, 500, base_url);
+      this.setState({loading: true});
+      return;
+		})
+	}
+
+	render() {
+		return (
+			<div className="App" style={{textAlign: "center"}}>
+				<Grid container spacing={24}
+				  	justify="center"
+				  	alignItems="center"
+				>
+					<Grid item xs={6}>
+						<input
+							accept="audio/*"
+							id="contained-big-file"
+							type="file"
+							style={{display: 'none' }}
+							onChange={this.handleselectedBigFile}
+						/>
+						<label htmlFor="contained-big-file">
+							<Button variant="contained" component="span" disabled={this.state.loading}>
+								Upload Big File
+							</Button>
+							{this.state.loading && <CircularProgress size={24} color="secondary"/>}
+							<label>
+								{this.state.bigFile ? this.state.bigFile.name: "No file selected"}
+							</label>
+						</label>
+					</Grid>
+					<Grid item xs={6}>
+						<input
+							accept="audio/*"
+							id="contained-little-file"
+							type="file"
+							style={{display: 'none' }}
+							onChange={this.handleselectedLittleFile}
+						/>
+						<label htmlFor="contained-little-file">
+							<Button variant="contained" component="span" disabled={this.state.loading}>
+								Upload Little File
+							</Button>
+							{this.state.loading && <CircularProgress size={24} color="secondary"/>}
+							<label>
+								{this.state.littleFile ? this.state.littleFile.name: "No file selected"}
+							</label>
+						</label>
+					</Grid>
+					<Button variant="contained" color="primary" onClick={this.handleUpload} disabled={this.state.loading} style={{marginRight: "30px"}}>
+						Send
+        				<Send />
+      				</Button>
+					{this.state.loading && <CircularProgress size={24} color="secondary"/>}
+					<Grid item xs={12}>
+						{this.state.loading && <LinearProgress variant="determinate" value={this.state.completed} />}
+					</Grid>
+				</Grid>
+				{this.state.resultsApi &&
+					<div>
+						<Grid container spacing={24}
+				  			justify="center"
+				  			alignItems="center"
+						>
+							<TextField
+								id="standard-name"
+								label="Time start of best precision"
+								value={this.state.resultsApi ? this.state.resultsApi.results.start_second: ""}
+								margin="normal"
+							/>
+							<TextField
+									id="standard-name"
+									label="Time start of best precision"
+									value={this.state.resultsApi ? this.state.resultsApi.results.end_second: ""}
+									margin="normal"
+							/>
+							<Grid item xs={12}>
+								<img alt="" src={this.state.resultsApi.results.distances_overlapping_img} />
+							</Grid>
+							<Grid item xs={12}>
+								<img alt="" src={this.state.resultsApi.results.best_adjust_overlapping_img} />
+							</Grid>
+						</Grid>
+					</div>
+				}
+			</div>
+		);
+	}
+}
+
+export default App;
